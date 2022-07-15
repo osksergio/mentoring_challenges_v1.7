d7e3a7f3cfaf165249eb1c3d786be86c2b0b26b3
Sergio Oseko <osk.sergio@gmail.com>
2022-07-14 23:01:59 -0300

    feat(suppliers): implemented filters

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -4,6 +4,11 @@ class SuppliersController < ApplicationController
   # GET /suppliers or /suppliers.json
   def index
     @suppliers = Supplier.all
+
+    # filter by ID
+    @suppliers = Supplier.where("id = #{params[:id]}") if params[:id]
+    # filter by Description (supplier's name)
+    @suppliers = Supplier.where("id = #{params[:id]}") if params[:id]
   end
 
   # GET /suppliers/1 or /suppliers/1.json
