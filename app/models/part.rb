49ab640013461bab9a929aebe2edd5e6cede14a4
Sergio Oseko <osk.sergio@gmail.com>
2022-07-19 20:21:55 -0300

    feat(parts): definition of associations between parts and suppliers

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,4 +1,5 @@
 class Part < ApplicationRecord
+  belongs_to :supplier
   validates :description, presence: true
   validates :description, length: { minimum: 3 }
 end
--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,5 +1,6 @@
 class Supplier < ApplicationRecord
   has_one :account
+  has_many :parts
   validates :description, presence: true
   validates :description, length: { minimum: 3, maximum: 60 }
 end
