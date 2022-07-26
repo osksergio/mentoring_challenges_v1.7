8f7b206c367d96ebab32e00db0a22d271ee1fed2
Sergio Oseko <osk.sergio@gmail.com>
2022-07-26 06:46:54 -0300

    test(books): implemented tests (requests #2)

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe "/books", type: :request do
     {
       title: "",
       published_at: nil,
-      author_id: -1
+      author_id: 1
     }
   }
 
@@ -35,7 +35,7 @@ RSpec.describe "/books", type: :request do
     end
   end
 
-  describe  "GET /show" do
+  describe "GET /show" do
     it "renders a successful response" do
       Author.create! params_author
       book = Book.create! valid_attributes
@@ -44,10 +44,59 @@ RSpec.describe "/books", type: :request do
     end
   end
 
-  describe  "GET /new" do
+  describe "GET /new" do
     it "renders a successful response" do
       get new_book_url
       expect(response).to be_successful
     end
   end
+
+  describe "GET /edit" do
+    it "renders a successful response" do
+      Author.create! params_author
+      book = Book.create! valid_attributes
+      get edit_book_url(book)
+      expect(response).to be_successful
+    end
+  end
+
+  describe "POST /create" do
+
+    # valid parameters
+    context "with valid parameters" do
+
+      it "creates a new Book" do
+        Author.create! params_author
+        expect {
+          post books_url, params: { book: valid_attributes }
+        }.to change(Book, :count).by(1)
+      end
+
+      it 'redirects to the created book' do
+        Author.create! params_author
+        post books_url, params: { book: valid_attributes }
+        expect(response).to redirect_to(book_url(Book.last))
+      end
+    end
+
+    # invalid parameters
+    context "with invalid parameters" do
+
+      it 'does not create a new Book' do
+        Author.create! params_author
+        expect {
+          post books_url, params: { book: invalid_attributes }
+        }.to change(Book, :count).by(0)
+      end
+
+      it "renders a successful response (i.e. to display the 'new' template)"  do
+        Author.create! params_author
+        post books_url, params: { book: invalid_attributes }
+        expect(response).to be_truthy
+      end
+    end
+
+  end
+
+
 end
