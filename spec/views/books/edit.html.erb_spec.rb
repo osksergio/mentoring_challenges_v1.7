966e69a4743d46a581b08184dc652ea25c335a89
Sergio Oseko <osk.sergio@gmail.com>
2022-07-28 06:47:56 -0300

    test(books): implemented tests (view - edit)

--- spec/views/books/edit.html.erb_spec.rb
+++ spec/views/books/edit.html.erb_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "books/edit", type: :view do
   before(:each) do
+    @author = assign(:author, Author.create!( description: "Agatha Christie" ))
+
     @book = assign(:book, Book.create!(
-      title: "MyString",
-      author: nil
+      title: "O Assassinato no Expresso Oriente",
+      author_id: 1
     ))
   end
 
@@ -12,9 +14,7 @@ RSpec.describe "books/edit", type: :view do
     render
 
     assert_select "form[action=?][method=?]", book_path(@book), "post" do
-
       assert_select "input[name=?]", "book[title]"
-
       assert_select "input[name=?]", "book[author_id]"
     end
   end
