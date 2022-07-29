f56111986a59fc9843f7e2d94cc4d71c2e86b171
Sergio Oseko <osk.sergio@gmail.com>
2022-07-29 07:03:55 -0300

    test(books): miscellaneous tests

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     title { "MyString" }
     published_at { "2022-07-14" }
     author { nil }
+    #author { FactoryBot.create(:author) }
   end
 end
--- spec/views/books/edit.html.erb_spec.rb
+++ spec/views/books/edit.html.erb_spec.rb
@@ -2,12 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "books/edit", type: :view do
   before(:each) do
-    @author = assign(:author, Author.create!( description: "Agatha Christie" ))
+    #@author = assign(:author, Author.create!( description: "Agatha Christie" ))
 
-    @book = assign(:book, Book.create!(
-      title: "O Assassinato no Expresso Oriente",
-      author_id: 1
-    ))
+    @author = FactoryBot.create(:author)
+
+    @book = FactoryBot.create(:book)
+
+    #@book = assign(:book, Book.create!(
+    #  title: "O Assassinato no Expresso Oriente",
+    #  author_id: 1
+    #))
   end
 
   it "renders the edit book form" do
@@ -15,6 +19,7 @@ RSpec.describe "books/edit", type: :view do
 
     assert_select "form[action=?][method=?]", book_path(@book), "post" do
       assert_select "input[name=?]", "book[title]"
+      assert_select "input[name=?]", "book[published_at]"
       assert_select "input[name=?]", "book[author_id]"
     end
   end
