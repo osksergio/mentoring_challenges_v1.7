6f4bd1a72c445f0ec85706115dc5271bbde493a5
Sergio Oseko <osk.sergio@gmail.com>
2022-07-31 09:43:25 -0300

    feat(authors): creating APIs

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,6 @@
 class AuthorsController < ApplicationController
   before_action :set_author, only: %i[ show edit update destroy ]
+  skip_before_action :verify_authenticity_token
 
   # GET /authors or /authors.json
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   resources :suppliers
   resources :books
   resources :authors
+
+  # APIs routes
+  #post 'authors/create'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
