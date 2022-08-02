65988cbd392678ee387665dd6ae1345f1e42ad40
Sergio Oseko <osk.sergio@gmail.com>
2022-08-02 07:13:13 -0300

    feat(books): creating APIs

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   before_action :set_book, only: %i[ show edit update destroy ]
+  skip_before_action :verify_authenticity_token
 
   # GET /books or /books.json
   def index
