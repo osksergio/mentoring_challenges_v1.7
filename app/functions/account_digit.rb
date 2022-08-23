c8b1e04212d02ac781bb81dcf9d8993c6d1ee2a1
Sergio Oseko <osk.sergio@gmail.com>
2022-08-23 06:53:50 -0300

    feat(accounts): function check digit (2)

--- app/functions/account_digit.rb
+++ app/functions/account_digit.rb
@@ -1,22 +1,23 @@
+module CHECKDIG
+  def check_digit(p_account, p_dig)
+    range = 0..( p_account.length() - 1 )
+    p_account = p_account.reverse
+    sum_product = 0
 
-def check_digit(p_account, p_dig)
-  range = 0..( p_account.length() - 1 )
-  p_account = p_account.reverse
-  sum_product = 0
+    for i in range
+      sum_product += p_account[i].to_i * (i + 2)
+    end
 
-  for i in range
-    sum_product += p_account[i].to_i * (i + 2)
-  end
+    sum_product = (sum_product * 10)
+    digit = sum_product % 11
 
-  sum_product = (sum_product * 10)
-  digit = sum_product % 11
+    #return digit
 
-  #return digit
-
-  if p_dig.to_i == digit
-    return  true
-  else
-    return false
+    if p_dig.to_i == digit
+      return  true
+    else
+      return false
+    end
   end
 end
 
--- app/models/account.rb
+++ app/models/account.rb
@@ -2,4 +2,29 @@ class Account < ApplicationRecord
   belongs_to :supplier
   validates :account_number, presence: true
   validates :account_number, uniqueness: true
+  validate :digit_is_valid?
+end
+
+def digit_is_valid?
+  # utilizada a gem 'isbn' para validação [https://github.com/tkersey/isbn  ]
+  errors.add(:digit, "Dígito verificador inválido! Por favor, verifique os dados.") unless check_digit(:account_number, :digit)
+end
+
+def check_digit(p_account, p_dig)
+  range = 0..( p_account.length() - 1 )
+  p_account = p_account.reverse
+  sum_product = 0
+
+  for i in range
+    sum_product += p_account[i].to_i * (i + 2)
+  end
+
+  sum_product = (sum_product * 10)
+  digit = sum_product % 11
+
+  if p_dig.to_i == digit
+    return  true
+  else
+    return false
+  end
 end
